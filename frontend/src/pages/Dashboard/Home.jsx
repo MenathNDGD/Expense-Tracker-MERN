8e02b3809088eec5a6b8dd78cee257299c32db84
MenathNDGD <nadungmenath1@gmail.com>
2025-03-06 20:43:17 +0530

    Wrap Home component with DashboardLayout for consistent layout structure

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto"></div>
+    </DashboardLayout>
+  );
 };
 
 export default Home;
