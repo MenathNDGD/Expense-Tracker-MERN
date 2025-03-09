d77edea08c1fa590f4354419ec8011a9c82ec9d6
MenathNDGD <nadungmenath1@gmail.com>
2025-03-09 19:00:33 +0530

    Implement deleteIncome function for income deletion with success and error handling

--- frontend/src/pages/Dashboard/Income.jsx
+++ frontend/src/pages/Dashboard/Income.jsx
@@ -78,7 +78,20 @@ const Income = () => {
     }
   };
 
-  const deleteIncome = async (id) => {};
+  const deleteIncome = async (id) => {
+    try {
+      await axiosInstance.delete(API_PATHS.INCOME.DELETE_INCOME(id));
+      setOpenDeleteAlert({ show: false, data: null });
+      toast.success("Income deleted successfully.");
+      fetchIncomeDetails();
+    } catch (error) {
+      console.error(
+        "Error deleting income: ",
+        error.response?.data?.message || error.message
+      );
+      toast.error("Error deleting income. Please try again later.");
+    }
+  };
 
   const handleDownloadIncomeDetails = async () => {};
 
