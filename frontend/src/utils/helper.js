1fd79324062a8396c38e6e4ddb3c199e0373e433
MenathNDGD <nadungmenath1@gmail.com>
2025-03-09 20:00:53 +0530

    Add helper function to prepare expense line chart data

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -53,3 +53,17 @@ export const prepareIncomeBarChartData = (data = []) => {
 
   return chartData;
 };
+
+export const prepareExpenseLineChartData = (data = []) => {
+  const sortData = [...data].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
+  const chartData = sortData.map((item) => ({
+    month: moment(item?.date).format("MMM"),
+    amount: item?.amount,
+    category: item?.category,
+  }));
+
+  return chartData;
+};
