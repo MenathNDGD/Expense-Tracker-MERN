2034cb4dd88238ec3b614c26a8170c3b2a2d32d2
MenathNDGD <nadungmenath1@gmail.com>
2025-03-09 17:42:14 +0530

    Add Toaster component for toast notifications in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
 
 import {
   BrowserRouter as Router,
@@ -30,6 +31,15 @@ const App = () => {
           </Routes>
         </Router>
       </div>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </UserProvider>
   );
 };
